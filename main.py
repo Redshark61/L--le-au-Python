ab99cf4dc198560fb368955b52de0e31ebdc700a
Redshark61 <tim.vannier@gmail.com>
2021-10-02 15:20:27 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fizz_buzz.fizz_buzz import fizzBuzz
 from number import number
 from map.map import map
+from functions.Clear import clear
 
 
 def main():
@@ -8,7 +9,9 @@ def main():
     print("1 - FizzBuzz")
     print("2 - Devine le nombre")
     print("3 - Afficher la carte")
+    print("q - Quitter")
     entry = input("")
+    clear()
 
     if entry == "1":
         fizzBuzz()
@@ -16,7 +19,11 @@ def main():
         number()
     elif entry == "3":
         map()
+    elif entry.upper() == "Q":
+        return
+    main()
 
 
 if __name__ == "__main__":
+    clear()
     main()
