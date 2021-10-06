fd8232295482451fc774121a2424484646708681
Redshark61 <tim.vannier@gmail.com>
2021-10-06 14:42:42 +0200

    english comments

--- map/displayMap.py
+++ map/displayMap.py
@@ -2,7 +2,6 @@ from functions.Colors import Colors
 from functions.Position import *
 from map.startQuest import startQuest
 from map.emojiDecoder import emojiDecoder
-import time
 
 
 def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: list, questDone: list) -> None:
@@ -10,48 +9,48 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
     Display the map using the map.json. Each number is for a particular color. By default, the characters are just 2 spaces
     but it can be emoji.
 
-    It also stores the different quest already done, and handle if it needs to be done or not.
+    It also stores the different quest already done, and handle if it needs to be played or not.
     """
 
     color = Colors
     color.init()
 
-    # On remonte le curseur de la taille de la map + la où est le curseur acutellement
+    # Move the cursor all the way to the top + where he currently is
     print((len(data)+3) * "\033[A", end="")
 
     row = 0
     map = ''
-    # Pour chaque ligne dans le json de la carte
+    # for each line of the map
     for i in data:
         col = 0
 
-        # Pour chaque case dans le json de la carte
+        # For each cell of the line
         for j in i:
-            # Le caractère de base est 2 espaces
+            # Main character is 2 spaces
             char = '\u0020\u0020'
 
-            # Si les coordonnés actuel sont une quête, mettre une coche verte si elle est faite
+            # If current cell is a played quest, then check it
             if len(questDone) > 0:
                 for quest in questDone:
                     if row == quest[1] and col == quest[0]:
-                        char = emojiDecoder("e29c85")
+                        char = emojiDecoder("e29c85")  # Green check
 
-            # Pour chaque position dans le json de coordonnés
+            # For every positioned element in the json
             for item in coord:
 
-                # Toutes les icônes doivent être converties de hex en string lisible par le terminal
+                # Every emoji needs to be convert from hex to string in order to properly display
                 symbol = emojiDecoder(coord[item]['mark'])
 
-                # Si ce n'est pas un joueur, on vérifie si la boucle affiche les coordonnés d'une position du json
+                # If the current cell is not a player, then we change its value to an emojie, if needed
                 if item != "player":
                     if row == coord[item]['coords'][1] and col == coord[item]['coords'][0]:
                         char = symbol
-                # Si c'est un joueur, on vérifie si la boucle affiche les coords du joueur
+                # If the current cell is the player, then we change its char to be the player's emoji
                 else:
                     if row == playerY and col == playerX:
                         char = symbol
 
-            # En fonction du code couleur de la case, on change le background
+            # Change the background depending on the color code of the json map
             if j == 1:
                 map += color.setBackground("brightGray", char)
             elif j == 2:
@@ -61,6 +60,7 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
             elif j == 4:
                 map += color.setBackground('brightYellow', char)
             elif j == 5:
+                # If the cell is a palm tree, but the player stand here, then dont display the tree, but player's emoji
                 if row == playerY and col == playerX:
                     map += color.setBackground('darkGray', char)
                 else:
@@ -73,9 +73,12 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
         row += 1
         map += '\n'
     print(map)
+
+    # Once the map is displayed, we check if the player's position is a quest to do
     for index, quest in enumerate(questToDo):
         quest = list(quest.keys())[0]
+        # If it is, then we play the quest
         if (playerY == questToDo[index][quest][1] and playerX == questToDo[index][quest][0]):
             questToDo, playerX = startQuest(coord, index, questToDo, playerX, playerY, quest, questDone)
-            return questToDo, playerX
+    # In either case we return the questToDo and the next player's position
     return questToDo, playerX
