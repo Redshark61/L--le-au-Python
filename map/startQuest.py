80c1a786aee8e3db579df838474c13297a3ac0e2
Redshark61 <tim.vannier@gmail.com>
2021-10-06 16:19:25 +0200

    indicator if a quest is just played, so that map is refreshed to display player next to the quest

--- map/startQuest.py
+++ map/startQuest.py
@@ -5,11 +5,12 @@ from functions.Position import clearBox
 def startQuest(coord, index, questToDo, playerX, playerY, quest, questDone):
     clearBox()
     module = __import__(f"{coord[quest]['folder']}.{coord[quest]['mainFile']}", fromlist=[None])
-    isWon = module.main()
-    if isWon:
+    hasWon = module.main()
+    isQuestDone = True
+    if hasWon:
         questDone.append(questToDo[index][quest])
         questToDo.pop(index)
         time.sleep(2)
         clearBox()
 
-    return questToDo, playerX+1
+    return questToDo, playerX+1, isQuestDone
