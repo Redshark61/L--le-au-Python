d937c4fe47743460215888708fac46ea0f06d27e
Redshark61 <tim.vannier@gmail.com>
2021-10-07 15:30:12 +0200

    the player cant walk on the sea or the water anymore + link with the different functions

--- map/displayMap.py
+++ map/displayMap.py
@@ -1,10 +1,12 @@
 from functions.Colors import Colors
 from functions.Position import *
 from map.startQuest import startQuest
-from map.emojiDecoder import emojiDecoder
+from functions.emojiDecoder import emojiDecoder
+from map.printMonkeys import printMonkeys
+from map.mapBackground import mapBackground
 
 
-def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: list, questDone: list) -> None:
+def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: list, questDone: list, prevPlayerX: int, prevPlayerY: int) -> None:
     """
     Display the map using the map.json. Each number is for a particular color. By default, the characters are just 2 spaces
     but it can be emoji.
@@ -44,6 +46,8 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
                 # If the current cell is the player, then we change its char to be the player's emoji
                 else:
                     if row == playerY and col == playerX:
+                        if j == 3 or j == 2:
+                            return questToDo, prevPlayerX, prevPlayerY, isQuestDone
                         char = symbol
 
             # If current cell is a played quest, then check it
@@ -53,22 +57,7 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
                         char = emojiDecoder("e29c85")  # Green check
 
             # Change the background depending on the color code of the json map
-            if j == 1:
-                map += color.setBackground("brightGray", char)
-            elif j == 2:
-                map += color.setBackground('brightCyan', char)
-            elif j == 3:
-                map += color.setBackground('darkBlue', char)
-            elif j == 4:
-                map += color.setBackground('brightYellow', char)
-            elif j == 5:
-                # If the cell is a palm tree, but the player stand here, then dont display the tree, but player's emoji
-                if row == playerY and col == playerX:
-                    map += color.setBackground('darkGray', char)
-                else:
-                    map += color.setBackground('darkGray', emojiDecoder('f09f8cb4'))
-            elif j == 6:
-                map += color.setBackground('darkGray', char)
+            map = mapBackground(color, char, map, j, row, col, playerX, playerY)
 
             col += 1
 
@@ -79,8 +68,13 @@ def displayMap(data: dict, coord: dict, playerX: int, playerY: int, questToDo: l
     # Once the map is displayed, we check if the player's position is a quest to do
     for index, quest in enumerate(questToDo):
         quest = list(quest.keys())[0]
+
         # If it is, then we play the quest
         if (playerY == questToDo[index][quest][1] and playerX == questToDo[index][quest][0]):
-            questToDo, playerX, isQuestDone = startQuest(coord, index, questToDo, playerX, playerY, quest, questDone)
+            if list(questToDo[index].keys())[0] == 'fizzBuzz':
+                printMonkeys(color, questToDo, index, quest)
+
+            questToDo, playerY, isQuestDone = startQuest(coord, index, questToDo, playerX, playerY, quest, questDone)
+
     # In either case we return the questToDo and the next player's position
-    return questToDo, playerX, isQuestDone
+    return questToDo, playerX, playerY, isQuestDone
