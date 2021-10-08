db09d67756dccb12faa7903ef2c7128a4b71d11a
Redshark61 <tim.vannier@gmail.com>
2021-10-08 14:53:25 +0200

    generic items are randomly created on the map

--- map/map.py
+++ map/map.py
@@ -9,6 +9,7 @@ from functions.drawFood import drawFood
 from functions.drawWater import drawWater
 from functions.drawEnergy import drawEnergy
 from map.getKeyPress import getKeyPress
+import random
 
 
 def map() -> None:
@@ -20,6 +21,18 @@ def map() -> None:
 
     # Récupérer les coordonnés
     coord = checkMod('coordinates')
+
+    items = checkMod("items")
+
+    createdItems = []
+    currentItems = items
+
+    for item in currentItems:
+        for i in range(currentItems[item]["number"]):
+            if currentItems[item]['spawn'] == "generic":
+                itemPosX = random.randint(10, 80)
+                itemPosY = random.randint(9, 20)
+                createdItems.append({item: [itemPosX, itemPosY]})
 
     # Coordonnés du joueur
     playerX = coord['player']["coords"][0]
@@ -43,32 +56,32 @@ def map() -> None:
     drawFood(foodMax)
     drawWater(waterMax)
     drawEnergy(energyMax)
+    print(position(3, 34, '-'*98))
 
     # Tant que le code de la touche pressé n'est pas 113 (q)
     while ord(char) != 113:
 
         # Afficher la carte
-        questToDo, playerX, playerY, isQuestDone = displayMap(data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY)
-
+        questToDo, playerX, playerY, isQuestDone, foodMax, waterMax, currentItems = displayMap(
+            data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY, foodMax, waterMax, createdItems, currentItems)
+        drawFood(foodMax)
+        drawWater(waterMax)
+        drawEnergy(energyMax)
         if isQuestDone:
-            displayMap(data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY)
+            displayMap(data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY, foodMax, waterMax, createdItems, currentItems)
             isQuestDone = False
 
         print(position(105, 2, "L'île aux Python !".center(47, ' ')))
         print(position(105, 3, "-"*47))
         print(position(105, 4, "1 - Dormir"))
 
-        char, prevPlayerX, prevPlayerY, playerX, playerY, energyMax, foodMax, waterMax = getKeyPress(
-            playerX, playerY, foodMax, waterMax, energyMax, data, coord, questToDo, questDone, prevPlayerX, prevPlayerY)
-
-        drawFood(foodMax)
-        drawWater(waterMax)
-        drawEnergy(energyMax)
+        char, prevPlayerX, prevPlayerY, playerX, playerY, energyMax, foodMax, waterMax, currentItems = getKeyPress(
+            playerX, playerY, foodMax, waterMax, energyMax, data, coord, questToDo, questDone, prevPlayerX, prevPlayerY, createdItems, currentItems)
 
         if foodMax < 0 or waterMax < 0 or energyMax < 0:
             print(position(105, 10, 'Vous êtes mort !'))
             playerFace = position(playerX*2+1, playerY+2, emojiDecoder('f09f9280'))
-            displayMap(data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY, playerFace)
+            displayMap(data, coord, playerX, playerY, questToDo, questDone, prevPlayerX, prevPlayerY,  foodMax, waterMax, createdItems, currentItems, playerFace)
             time.sleep(5)
             return
 
