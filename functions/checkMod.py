50389065c5c9f059bdaed274e8a4149f1c88262f
Redshark61 <tim.vannier@gmail.com>
2021-10-13 16:18:07 +0200

    addition of the encoding for the json file

--- functions/checkMod.py
+++ functions/checkMod.py
@@ -8,10 +8,10 @@ def checkMod(name: str) -> dict:
     from the mods folder if there is, or the data folder if ther is no mods
     """
     if os.path.isfile(f"mods/{name}.json"):
-        with open(f'mods/{name}.json') as file:
+        with open(f'mods/{name}.json', encoding='utf-8') as file:
             data = json.load(file)
     else:
-        with open(f"data/{name}.json") as file:
+        with open(f"data/{name}.json", encoding='utf-8') as file:
             data = json.load(file)
 
     return data
