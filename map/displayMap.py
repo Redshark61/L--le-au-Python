c8c9d41af1d6c5dd0e8643f8f92a2a8c6a0e7e5c
Redshark61 <tim.vannier@gmail.com>
2021-10-13 11:28:45 +0200

    fixing bug where player was actually outside the map but even though his emoji didnt move

--- map/displayMap.py
+++ map/displayMap.py
@@ -9,7 +9,7 @@ from functions import config
 
 def displayMap(playerFace: str = None) -> None:
     """
-    Display the map using the map.json. Each number is for a particular color. By default, the characters are just 2 spaces
+    Display the mapBg using the mapBg.json. Each number is for a particular color. By default, the characters are just 2 spaces
     but it can be emoji.
 
     It also stores the different quest already done, and handle if it needs to be played or not.
@@ -22,10 +22,10 @@ def displayMap(playerFace: str = None) -> None:
     print(position(1, 0, ''))
 
     row = 0
-    map = ''
+    mapBg = ''
     isQuestDone = False
 
-    # for each line of the map
+    # for each line of the mapBg
     for i in config.data:
         col = 0
 
@@ -61,6 +61,8 @@ def displayMap(playerFace: str = None) -> None:
                 else:
                     if row == config.playerCoord[1] and col == config.playerCoord[0]:
                         if j == 3 or j == 2:
+                            config.playerCoord[0] = config.prevPlayerCoord[0]
+                            config.playerCoord[1] = config.prevPlayerCoord[1]
                             return
                         elif playerFace is not None:
                             char = playerFace
@@ -72,16 +74,16 @@ def displayMap(playerFace: str = None) -> None:
                     if row == quest[1] and col == quest[0]:
                         config.char = emojiDecoder("e29c85")  # Green check
 
-            # Change the background depending on the color code of the json map
-            map = mapBackground(color, char, map, j, row, col)
+            # Change the background depending on the color code of the json mapBg
+            mapBg = mapBackground(color, char, mapBg, j, row, col)
 
             col += 1
 
         row += 1
-        map += '\n'
-    print(map)
+        mapBg += '\n'
+    print(mapBg)
 
-    # Once the map is displayed, we check if the player's position is a quest to do
+    # Once the mapBg is displayed, we check if the player's position is a quest to do
     for index, quest in enumerate(config.questToDo):
         quest = list(quest.keys())[0]
 
