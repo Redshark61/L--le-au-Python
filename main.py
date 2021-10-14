3aa3f6f1569b46bc857d536909ae61a975ff23b6
Redshark61 <tim.vannier@gmail.com>
2021-10-14 11:27:29 +0200

    begining of the save process

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import os.path
 from map.mapLoop import mapLoop
 from functions.Clear import clear
 from functions import config
@@ -5,7 +7,21 @@ from functions import config
 
 def main() -> None:
     # Begining of the program
-    print(config.hiddenCursor)
+
+    if os.path.isfile('./save.json'):
+        with open('save.json', encoding='utf-8') as f:
+            save = json.load(f)
+
+        print(f"Il y a déjà une sauvegarde au nom de {save['playerName']}. La charger ?\n")
+        choice = input("1 - oui\n2 - non\n")
+        match choice:
+            case '1':
+                pass
+            case '2':
+                playerName = input("Quel est ton nom :\n")
+                with open('save.json', 'w', encoding='utf-8') as f:
+                    json.dump({"playerName": playerName}, f)
+                print(config.hiddenCursor)
     mapLoop()
     clear()
 
