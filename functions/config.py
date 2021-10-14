1c76e0220cd48621045e8e96edb78af4f1e7c157
Redshark61 <tim.vannier@gmail.com>
2021-10-14 11:14:11 +0200

    new comments

--- functions/config.py
+++ functions/config.py
@@ -1,13 +1,18 @@
+# Map data
 coord, data, playerCoord, questToDo, prevPlayerCoord, questDone, createdItems, currentItems, pickedUpItem = [], [], [], [], [], [], [], [], []
 char = ' '
-itemSelected, toBePosition, currentPosition = 0, 0, 0
 
+# Inventory
+itemSelected, toBePosition, currentPosition = 0, 0, 0
 noDuplicateInventory = []
+
+# Health
 vitalSigns = {
     "energyMax": 100,
     "foodMax": 100,
     "waterMax": 100
 }
 
+# Cursors
 visibleCursor = '\033[?25h'
 hiddenCursor = '\033[?25l'
