aeafef6f50a41af233a078f78a8f82ab8936667d
Redshark61 <tim.vannier@gmail.com>
2021-10-15 11:16:16 +0200

    items doesnt spawn in water anymore

--- map/randomItemPosition.py
+++ map/randomItemPosition.py
@@ -13,15 +13,20 @@ def randomItemPosition() -> list[dict]:
     ]
     """
     items = checkMod("items")
+    mapCoord = checkMod("map")
 
     createdItems = []
     currentItems = items
-
+    # For each item wich needs to be created
     for item in currentItems:
+        # We create the number of item desired
         for _ in range(currentItems[item]["number"]):
+            itemPosX = 0
+            itemPosY = 0
             if currentItems[item]['spawn'] == "generic":
-                itemPosX = random.randint(10, 42)
-                itemPosY = random.randint(9, 20)
+                while mapCoord[itemPosY][itemPosX] == 2 or mapCoord[itemPosY][itemPosX] == 3:
+                    itemPosX = random.randint(0, len(mapCoord[0])-1)
+                    itemPosY = random.randint(0, len(mapCoord)-1)
                 createdItems.append({item: [itemPosX, itemPosY]})
 
     return createdItems, currentItems
