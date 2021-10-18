42aa37206439cc533e86c8f81f36673cf5ec6547
Redshark61 <tim.vannier@gmail.com>
2021-10-18 11:20:38 +0200

    fix map display problem when the player slept

--- map/getKeyPress.py
+++ map/getKeyPress.py
@@ -35,7 +35,7 @@ def getKeyPress(inventoryOpen: bool, noDuplicateInventory: list[tuple]) -> Union
     elif ord(config.char) == 49 and not inventoryOpen:  # 1
         config.char = ' '
         print(position(105, 9, 'Je dors...' + ' ' * 10))
-        playerFace = position(config.playerCoord[0] * 2 + 1, config.playerCoord[1] + 2, emojiDecoder('f09f98b4'))
+        playerFace = position(config.playerCoord[0] * 2 + 1, config.playerCoord[1] + 1, emojiDecoder('f09f98b4'))
         displayMap(playerFace)
         while config.vitalSigns["energyMax"] < 100 and config.vitalSigns['foodMax'] > 20:
             config.vitalSigns["energyMax"] = gainEnergy(config.vitalSigns["energyMax"])
