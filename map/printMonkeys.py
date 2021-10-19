6aa90dcc34a15c971d401e5cc615da144f65093f
Redshark61 <tim.vannier@gmail.com>
2021-10-19 09:47:20 +0200

    changed the postiton of monkeys

--- map/printMonkeys.py
+++ map/printMonkeys.py
@@ -4,7 +4,7 @@ import functions.config as config
 
 
 def printMonkeys(color, index, quest):
-    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2-1, (config.questToDo[index][quest][1])+2, emojiDecoder('f09f90b5'))))
-    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+3, (config.questToDo[index][quest][1])+2, emojiDecoder('f09f90b5'))))
-    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+1, (config.questToDo[index][quest][1])+1, emojiDecoder('f09f90b5'))))
-    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+1, (config.questToDo[index][quest][1]+3), emojiDecoder('f09f90b5'))))
+    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2-1, (config.questToDo[index][quest][1])+1, emojiDecoder('f09f90b5'))))
+    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+3, (config.questToDo[index][quest][1])+1, emojiDecoder('f09f90b5'))))
+    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+1, (config.questToDo[index][quest][1]), emojiDecoder('f09f90b5'))))
+    print(color.setBackground('darkGreen', position((config.questToDo[index][quest][0])*2+1, (config.questToDo[index][quest][1]+2), emojiDecoder('f09f90b5'))))
